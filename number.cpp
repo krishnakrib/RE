c061270ad3451089c990968ea9833e48ab72094c
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-02-14 09:59:19 +0530

    Create number.cpp

---
+++ number.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number;
+printf("enter the number:");
+scanf("%d",&number);
+if(num>0)
+printf("%d is positive")
+if(num<0)
+printf("%d is negative")
+else
+printf("%d is zero")
