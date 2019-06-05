ffb9767d1ec301ef606c3e401a6bde41cd635f7a
krishnakrib <42169952+krishnakrib@users.noreply.github.com>
2019-06-05 13:06:17 +0530

    Create arm.py

---
+++ arm.py
@@ -0,0 +1,20 @@
+import math
+def findarmstrong(low,high):
+    for i in range(low+1,high):
+        x=i
+        n=0
+        while(x!=0)
+             x=x/10
+             n=n+1
+       pow_sum=0
+       x=i
+       while(x!=0)
+            digit=x%10
+            pow_sum=sum+math.pow(digit,n)
+            x=x/10
+       if(pow_sum==i)
+            print(str(i)+" ")
+num1=100
+num2=400
+findarmstrong(num 1,num2)
+print(" ")
